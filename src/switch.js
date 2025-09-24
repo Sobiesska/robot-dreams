f7662b22e98396138f05f98439b54fbef9b2965c
podolska-li-ma <lilia.po@moonactive.com>
2025-09-24 12:08:57 +0100

    Refactor package.json and package-lock.json to remove unused dependencies and scripts; enhance switch.js with detailed logging for day type determination

--- src/switch.js
+++ src/switch.js
@@ -3,18 +3,33 @@ let dayType = 'weekend' | 'weekday' | 'unknown day';
 
 switch (day) {
   case 'saturday':
+    dayType = 'weekend';
+    console.log(`${day} ->`, dayType);
+    break;
   case 'sunday':
     dayType = 'weekend';
+    console.log(`${day} ->`, dayType);
     break;
   case 'monday':
+    dayType = 'weekday';
+    console.log(`${day} ->`, dayType);
+    break;
   case 'tuesday':
+    dayType = 'weekday';
+    console.log(`${day} ->`, dayType);
+    break;
   case 'wednesday':
+    dayType = 'weekday';
+    console.log(`${day} ->`, dayType);
+    break;
   case 'thursday':
+    dayType = 'weekday';
+    console.log(`${day} ->`, dayType);
+    break;
   case 'friday':
     dayType = 'weekday';
+    console.log(`${day} ->`, dayType);
     break;
   default:
     dayType = 'unknown day';
 }
-
-console.log(`${day} ->`, dayType);
