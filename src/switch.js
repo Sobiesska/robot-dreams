1223133aee18e1d06718db2033c47d097d26ce78
podolska-li-ma <lilia.po@moonactive.com>
2025-09-24 14:42:06 +0100

    Fix linter

--- src/switch.js
+++ src/switch.js
@@ -2,34 +2,34 @@ const day = 'monday';
 let dayType = 'weekend' | 'weekday' | 'unknown day';
 
 switch (day) {
-  case 'saturday':
-    dayType = 'weekend';
-    console.log(`${day} ->`, dayType);
-    break;
-  case 'sunday':
-    dayType = 'weekend';
-    console.log(`${day} ->`, dayType);
-    break;
-  case 'monday':
-    dayType = 'weekday';
-    console.log(`${day} ->`, dayType);
-    break;
-  case 'tuesday':
-    dayType = 'weekday';
-    console.log(`${day} ->`, dayType);
-    break;
-  case 'wednesday':
-    dayType = 'weekday';
-    console.log(`${day} ->`, dayType);
-    break;
-  case 'thursday':
-    dayType = 'weekday';
-    console.log(`${day} ->`, dayType);
-    break;
-  case 'friday':
-    dayType = 'weekday';
-    console.log(`${day} ->`, dayType);
-    break;
-  default:
-    dayType = 'unknown day';
+    case 'saturday':
+        dayType = 'weekend';
+        console.log(`${day} ->`, dayType);
+        break;
+    case 'sunday':
+        dayType = 'weekend';
+        console.log(`${day} ->`, dayType);
+        break;
+    case 'monday':
+        dayType = 'weekday';
+        console.log(`${day} ->`, dayType);
+        break;
+    case 'tuesday':
+        dayType = 'weekday';
+        console.log(`${day} ->`, dayType);
+        break;
+    case 'wednesday':
+        dayType = 'weekday';
+        console.log(`${day} ->`, dayType);
+        break;
+    case 'thursday':
+        dayType = 'weekday';
+        console.log(`${day} ->`, dayType);
+        break;
+    case 'friday':
+        dayType = 'weekday';
+        console.log(`${day} ->`, dayType);
+        break;
+    default:
+        dayType = 'unknown day';
 }
