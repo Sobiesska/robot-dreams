4c91b74ae2015693cefc92982ea39c1c7e34b725
podolska-li-ma <lilia.po@moonactive.com>
2025-10-08 22:14:31 +0300

    Homework for lesson 7

---
+++ lesson_7/src/arrow-functions.ts
@@ -0,0 +1,19 @@
+const stringArray = ['string1', 'string2', 'string3', 'string4', 'string5'];
+const numberArray = [1, 2, 3, 4, 5];
+const combinedArray = [...stringArray, ...numberArray];
+
+export const sumArrayItems = async (array: (string | number)[]): Promise<string | number> => {
+    if (array.every(item => typeof item === 'number')) {
+        return (array as number[]).reduce((accumulator, current) => accumulator + current);
+    } else if (array.every(item => typeof item === 'string')) {
+        return (array as string[]).reduce((accumulator, current) => accumulator + current);
+    } else {
+        return array.reduce((accumulator, current) => accumulator.toString() + current.toString());
+    }
+};
+
+const sumNumbers = sumArrayItems(numberArray);
+const sumStringsAndNumbers = sumArrayItems(combinedArray);
+
+console.log(sumNumbers);
+console.log(sumStringsAndNumbers);
---
+++ lesson_7/src/functions.ts
@@ -0,0 +1,16 @@
+const stringArray = ['string1', 'string2', 'string3', 'string4', 'string5'];
+const numberArray = [1, 2, 3, 4, 5];
+const combinedArray = [...stringArray, ...numberArray];
+
+async function sumArrayItems(array: (string | number)[]): Promise<string | number> {
+    if (array.every((item) => typeof item === 'number')) {
+        return (array as number[]).reduce((accumulator, current) => accumulator + current);
+    } else if (array.every((item) => typeof item === 'string')) {
+        return (array as string[]).reduce((accumulator, current) => accumulator + current);
+    } else {
+        return array.reduce((accumulator, current) => accumulator.toString() + current.toString());
+    }
+}
+
+console.log(sumArrayItems(numberArray));
+console.log(sumArrayItems(combinedArray));
