0eeb8b3dc8350cad81ebb1dddd13dc652633cb3c
podolska-li-ma <lilia.po@moonactive.com>
2025-10-16 15:44:32 +0300

    Homework 9

---
+++ lesson_9/src/objects/dog.ts
@@ -0,0 +1,45 @@
+import { IAnimal} from '../abstractions/i-animal';
+
+export class Dog implements IAnimal {
+    public type: string;
+    public name: string;
+    public age: number;
+    public sound: string;
+    public color: string;
+    public weight: number;
+    public speed: number;
+    public food: string;
+    public isHunting: boolean;
+
+    public constructor() {
+        this.type = 'dog';
+        this.name = 'Mia';
+        this.age = 1;
+        this.sound = 'woof-woof';
+        this.color = 'brown';
+        this.weight = 10.5;
+        this.speed = 10;
+        this.food = 'bones';
+        this.isHunting = true;
+    }
+
+    public eat(): void {
+        console.log(`${this.name} ${this.type} is eating ${this.food}`);
+    }
+
+    public sleep(): void {
+        console.log(`${this.name} ${this.type} is sleeping`);
+    }
+
+    public play(): void {
+        console.log(`${this.name} ${this.type} is playing`);
+    }
+
+    public makeSound(): void {
+        console.log(`${this.name} ${this.type} is making sound ${this.sound}`);
+    }
+
+    public hunt(): void {
+        console.log(`${this.name} ${this.type} is hunting cat`);
+    }
+}
